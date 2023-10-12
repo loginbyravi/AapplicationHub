7e6baffa0ad30fa38de1ae54bca020cbed21d1d6
Ravi <loginbyravi@gmail.com>
2023-10-12 22:23:19 +0530

    First commit 1

---
+++ loginpage-work/loginpage/src/test/java/com/rkant/loginpage/LoginpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkant.loginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
